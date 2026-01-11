b325ffd5a54857b3667bcead800626ae9749aeed
Elgedawy31 <mohamedelgedawy40@gmail.com>
2026-01-11 12:02:27 +0200

    style(Footer): adjust grid layout for improved responsiveness
    - Changed the grid layout in the Footer component from 4 columns to 3 columns for better visual consistency and responsiveness across different screen sizes.

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -29,10 +29,9 @@ export default function Footer() {
 
         {/* Grid */}
         <div
-          className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 ${
+          className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ${
             isRTL ? "text-right" : "text-left"
-          }`}
-        >
+          }`}>
           {/* Quick Links */}
           <div className="space-y-6">
             <h3 className="text-lg font-semibold">{t("quickLinks")}</h3>
